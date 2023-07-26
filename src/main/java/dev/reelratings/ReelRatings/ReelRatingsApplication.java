94de5ad93b068afc4955d018eed238369b2c0b44
Arshdeep Chauhan <avc.swe17@gmail.com>
2023-07-25 18:03:37 -0700

    Init springboot commit.

---
+++ src/main/java/dev/reelratings/ReelRatings/ReelRatingsApplication.java
@@ -0,0 +1,13 @@
+package dev.reelratings.ReelRatings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReelRatingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReelRatingsApplication.class, args);
+	}
+
+}
