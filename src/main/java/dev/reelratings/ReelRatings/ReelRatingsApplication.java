a9bcc4697022c3bf86201398fa1709c4dd328917
Arshdeep Chauhan <avc.swe17@gmail.com>
2023-08-13 16:01:03 -0700

    At app init, exclude Mongo config check.

--- src/main/java/dev/reelratings/ReelRatings/ReelRatingsApplication.java
+++ src/main/java/dev/reelratings/ReelRatings/ReelRatingsApplication.java
@@ -2,8 +2,11 @@ package dev.reelratings.ReelRatings;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		MongoAutoConfiguration.class
+})
 public class ReelRatingsApplication {
 
 	public static void main(String[] args) {
