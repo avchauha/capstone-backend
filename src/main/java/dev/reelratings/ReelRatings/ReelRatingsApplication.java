7d67049660e37f7b2b1234ed84d9f42e1ec0bbc9
Arshdeep Chauhan <avc.swe17@gmail.com>
2023-08-13 21:43:38 -0700

    Init global cors config.

--- src/main/java/dev/reelratings/ReelRatings/ReelRatingsApplication.java
+++ src/main/java/dev/reelratings/ReelRatings/ReelRatingsApplication.java
@@ -3,14 +3,30 @@ package dev.reelratings.ReelRatings;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication(exclude = {
-		MongoAutoConfiguration.class
-})
+
+@SpringBootApplication
 public class ReelRatingsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReelRatingsApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("https://reelratingsapp-frontendfinal.onrender.com")
+						.allowedMethods("*")
+						.allowedHeaders("*")
+						.allowCredentials(false).maxAge(3600);
+			}
+		};
 	}
 
 }
