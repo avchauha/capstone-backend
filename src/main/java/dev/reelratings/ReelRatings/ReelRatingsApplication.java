de488af2824836c10308ab282eaa28d43c308070
Arshdeep Chauhan <avc.swe17@gmail.com>
2023-08-15 10:55:32 -0700

    Trying localhost

--- src/main/java/dev/reelratings/ReelRatings/ReelRatingsApplication.java
+++ src/main/java/dev/reelratings/ReelRatings/ReelRatingsApplication.java
@@ -15,17 +15,4 @@ public class ReelRatingsApplication {
 		SpringApplication.run(ReelRatingsApplication.class, args);
 	}
 
-//	@Bean
-//	public WebMvcConfigurer corsConfigurer() {
-//		return new WebMvcConfigurer() {
-//			@Override
-//			public void addCorsMappings(CorsRegistry registry) {
-//				registry.addMapping("/**")
-//						.allowedOrigins("*")
-//						.allowedMethods("*")
-//						.allowedHeaders("*");
-//			}
-//		};
-//	}
-
 }
